ea5beda532b3a22d426d990d1b4d9a4bb31d5cd4
Molly <dolanmollye@gmail.com>
2020-12-10 11:29:14 -0800

    complete

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/name' do 
+        "My name is Molly Dolan"
+    end
+
+    get '/hometown' do
+        "My hometown is Seattle, WA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Real Love' by Mary J. Blige"
+    end
 end
